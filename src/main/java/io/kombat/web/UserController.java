a2fad856fdb7aa06eb4e697116e6d295596c1a31
finx <oieusouofinx@gmail.com>
2015-11-26 16:42:57 -0200

    controllers, services and view almost working

--- src/main/java/io/kombat/web/UserController.java
+++ src/main/java/io/kombat/web/UserController.java
@@ -33,138 +33,134 @@ import io.kombat.domain.services.UserService;
 @Controller("/users")
 public class UserController {
 
-    @Inject
-    private Validator validator;
+	@Inject
+	private Validator validator;
 
-    @Inject
-    private UserService service;
+	@Inject
+	private UserService service;
 
-    public void current(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        try {
-            request.setAttribute("current", service.one(Long.parseLong((String) request.getAttribute("id"))));
-        } catch (SQLException e) {
-            response.sendError(404);
-        }
-    }
+	public void current(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		try {
+			request.setAttribute("current", service.one(Long.parseLong((String) request.getAttribute("id"))));
+		} catch (SQLException e) {
+			response.sendError(404);
+		}
+	}
 
-    public void user(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        HttpRequest req = (HttpRequest) request;
-        User user = new User();
+	public void user(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		HttpRequest req = (HttpRequest) request;
+		User user = new User();
 
-        Map<String, Object> params = req.getParams();
-        user.setEmail((String) params.get("email"));
-        user.setName((String) params.get("name"));
-//        user.setPassword((String) params.get("password"));
+		Map<String, Object> params = req.getParams();
+		user.setName((String) params.get("name"));
+		user.setEmail((String) params.get("email"));
+		user.setPicture((String) params.get("picture"));
 
-        HttpSession session = request.getSession();
+		HttpSession session = request.getSession();
 
-        Set<ConstraintViolation<User>> validate = validator.validate(user);
+		Set<ConstraintViolation<User>> validate = validator.validate(user);
 
-        if (validate.size() > 0) {
-            Map<String, List<String>> validations = new LinkedHashMap<String, List<String>>();
-            for (ConstraintViolation<User> validation : validate) {
+		if (validate.size() > 0) {
+			Map<String, List<String>> validations = new LinkedHashMap<String, List<String>>();
+			for (ConstraintViolation<User> validation : validate) {
 
-                String key = validation.getPropertyPath().toString();
-                List<String> value = validations.get(key);
+				String key = validation.getPropertyPath().toString();
+				List<String> value = validations.get(key);
 
-                if (value == null) {
-                    value = new ArrayList<String>();
-                }
-                value.add(validation.getMessage());
-                validations.put(key, value);
+				if (value == null) {
+					value = new ArrayList<String>();
+				}
+				value.add(validation.getMessage());
+				validations.put(key, value);
 
-            }
+			}
 
-            session.setAttribute("flash.validations", validations);
-            session.setAttribute("flash.user", user);
-            throw new HttpException(400, request.getHeader("referer"));
-        }
+			session.setAttribute("flash.validations", validations);
+			session.setAttribute("flash.user", user);
+			throw new HttpException(400, request.getHeader("referer"));
+		}
 
-        request.setAttribute("user", user);
-    }
+		request.setAttribute("user", user);
+	}
 
-    @Before(value = {"current", "user"})
-    @PUT("/:id")
-    public void save(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Before(value = { "current", "user" })
+	@PUT("/:id")
+	public void save(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		User current = (User) request.getAttribute("current");
+		User user = (User) request.getAttribute("user");
+		String uri = String.format("/users/%d", current.getId());
+		HttpSession session = request.getSession();
 
-        User current = (User) request.getAttribute("current");
-        User user = (User) request.getAttribute("user");
-        String uri = String.format("/users/%d", current.getId());
-        HttpSession session = request.getSession();
+		try {
+			current.setEmail(user.getEmail());
+			current.setName(user.getName());
+			current.setPicture(user.getPicture());
+			service.save(current);
 
-        try {
+			session.setAttribute("flash.notice", "User saved with success.");
+			response.sendRedirect(uri);
+		} catch (SQLException e) {
+			session.setAttribute("flash.user", user);
+			session.setAttribute("flash.warning", e.getMessage());
+			response.sendRedirect(uri);
+		}
 
-            current.setEmail(user.getEmail());
-            current.setName(user.getName());
-//            current.setPassword(user.getPassword());
-            service.save(current);
+	}
 
-            session.setAttribute("flash.notice", "User saved with success.");
-            response.sendRedirect(uri);
+	@DELETE("/:id")
+	public void destroy(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uri = "/users";
+		HttpSession session = request.getSession();
+		try {
+			service.destroy(Long.parseLong((String) request.getAttribute("id")));
+			session.setAttribute("flash.notice", "User removed with success.");
+			response.sendRedirect(uri);
 
-        } catch (SQLException e) {
-            session.setAttribute("flash.user", user);
-            session.setAttribute("flash.warning", e.getMessage());
-            response.sendRedirect(uri);
-        }
+		} catch (SQLException e) {
+			session.setAttribute("flash.warning", e.getMessage());
+			response.sendRedirect(uri);
+		}
+	}
 
-    }
+	@Before("user")
+	@POST
+	public void create(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		User user = (User) request.getAttribute("user");
+		String uri = "/users/new";
+		HttpSession session = request.getSession();
 
-    @DELETE("/:id")
-    public void destroy(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String uri = "/users";
-        HttpSession session = request.getSession();
-        try {
-            service.destroy(Long.parseLong((String) request.getAttribute("id")));
-            session.setAttribute("flash.notice", "User removed with success.");
-            response.sendRedirect(uri);
+		try {
+			service.create(user);
+			session.setAttribute("flash.notice", "User created with success.");
+			response.sendRedirect("/users");
+		} catch (SQLException e) {
+			session.setAttribute("flash.user", user);
+			session.setAttribute("flash.warning", e.getMessage());
+			response.sendRedirect(uri);
+		}
+	}
 
-        } catch (SQLException e) {
-            session.setAttribute("flash.warning", e.getMessage());
-            response.sendRedirect(uri);
-        }
-    }
+	@GET("/new")
+	public void form(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/users/new.jsp").forward(request, response);
+	}
 
-    @Before("user")
-    @POST
-    public void create(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@GET
+	public void index(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			request.setAttribute("users", service.fetch(0, -1));
+		} catch (SQLException e) {
+			request.setAttribute("error", String.format("An unexpected error ocurred: %s", e.getMessage()));
+		}
 
-        User user = (User) request.getAttribute("user");
-        String uri = "/users/new";
-        HttpSession session = request.getSession();
+		request.getRequestDispatcher("/WEB-INF/views/users/index.jsp").forward(request, response);
 
-        try {
-            service.create(user);
-            session.setAttribute("flash.notice", "User created with success.");
-            response.sendRedirect("/users");
-        } catch (SQLException e) {
-            session.setAttribute("flash.user", user);
-            session.setAttribute("flash.warning", e.getMessage());
-            response.sendRedirect(uri);
-        }
-    }
+	}
 
-    @GET("/new")
-    public void form(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/views/users/new.jsp").forward(request, response);
-    }
-
-    @GET
-    public void index(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try {
-            request.setAttribute("users", service.fetch(0, -1));
-        } catch (SQLException e) {
-            request.setAttribute("error", String.format("An Error ocurred %s", e.getMessage()));
-        }
-
-        request.getRequestDispatcher("/WEB-INF/views/users/index.jsp").forward(request, response);
-
-    }
-
-    @Before("current")
-    @GET("/:id")
-    public void show(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/views/users/show.jsp").forward(request, response);
-    }
+	@Before("current")
+	@GET("/:id")
+	public void show(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/users/show.jsp").forward(request, response);
+	}
 
 }
